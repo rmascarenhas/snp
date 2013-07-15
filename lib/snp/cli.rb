9d3eb8892f8ddb13a249c6eb3df8ec513a88a133
Renato Mascarenhas <mascarenhas.renato@gmail.com>
2013-07-15 00:00:01 -0300

    Add documentation for CLI.

--- lib/snp/cli.rb
+++ lib/snp/cli.rb
@@ -27,6 +27,10 @@ module Snp
     end
   end
 
+  # Snp::InvalidOptions
+  #
+  # This exception is raised when there is an error parsing command line options
+  # passed to `snp`.
   class InvalidOptions < StandardError
     def initialize(invalid_option)
       super("Invalid option: #{invalid_option}")
@@ -43,16 +47,29 @@ module Snp
   #
   #   CLI.parse_options # => ['/Users/john/.snp/jquery.html.erb', { version: '1.9' }]
   class CLI
+    # Public: extract template name and other data that should be used to compile the
+    # template.
+    #
+    # arguments - an array of arguments that should be parsed. Defaults to `ARGV`.
+    #
+    # This method returns an array in which the first element is the template name and the
+    # second are the data that should be used to compile the template.
     def self.parse_options(arguments = ARGV.dup)
       new(arguments).parse
     end
 
+    # Internal: creates a new `Snp::CLI` instance.
+    #
+    # params - array of arguments.
+    # stream - the stream to be used when there is feedback to be given to the user. This
+    #          object must respond to `out` and `err` for normal and error situations.
     def initialize(params, stream = StandardStream.new)
       @params  = params
       @options = {}
       @stream  = stream
     end
 
+    # Internal: actually does the parsing job.
     def parse
       template_name = parse_static_options
       template_data = parse_dynamic_options
@@ -67,11 +84,20 @@ module Snp
 
     private
 
+    # Internal: parses the command line options to check for static options
+    # (version number and help).
     def parse_static_options
       option_parser.parse!(@params)
       @params.pop
     end
 
+    # Internal: parses dynamic options, creating options according to the arguments
+    # passed on the command line.
+    #
+    # Example
+    #
+    #   # command is '--project snp --language ruby template_name'
+    #   parse_dynamic_options # => { 'project' => 'snp', 'language' => 'ruby' }
     def parse_dynamic_options
       if @params.empty?
         {}
@@ -89,6 +115,8 @@ module Snp
       end
     end
 
+    # Internal: builds the static option parser. Recognizes `-V` for version and `-h`
+    # for help.
     def option_parser
       @_option_parser ||= Slop.new do |command|
         command.banner "Usage: #{program_name} [options] [template_name]"
@@ -103,15 +131,23 @@ module Snp
       end
     end
 
+    # Internal: builds the dynamic option parser, that creates options on the fly according
+    # to the passed command line options.
     def dynamic_parser
       @_dynamic_parser ||= Slop.new(autocreate: true)
     end
 
+    # Internal: prints a message and exits.
+    #
+    # message - the message to be printed before exiting.
+    #
+    # This method finishes the current process with a success exit status.
     def print_and_exit(message)
       @stream.out message
       exit
     end
 
+    # Internal: the program name to be used when generating output to the user.
     def program_name
       File.basename($0, '.*')
     end
